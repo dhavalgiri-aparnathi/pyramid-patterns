23bbcee00f92eaebe35a32a5647dc16e7c4bf520
dhavalgiri-aparnathi <dhavalgiriaparnathi000@gmail.com>
2025-01-22 10:10:44 +0530

    just one pyramid

---
+++ 41_pyramid.cpp
@@ -0,0 +1,30 @@
+/*
+
+        1
+      2 1
+    3 2 1
+  4 3 2 1
+5 4 3 2 1
+
+*/
+
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int space = i; space <= n; space++)
+        {
+            cout << "  ";
+        }
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
